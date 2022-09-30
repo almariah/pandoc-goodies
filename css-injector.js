387989a810ff751683b5c38020d8718eea505658
Abdullah Almariah <abdullahalmariah@gmail.com>
2022-09-30 13:54:09 +0200

    init

--- css-injector.js
+++ css-injector.js
@@ -1,20 +1,3 @@
-/*
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-GitHub HTML5 Pandoc Template: Inject CSS v1.0 (2017-11-21)
-(c) Tristano Ajmone, 2017. MIT License (MIT).
-https://github.com/tajmone/pandoc-goodies
-//////////////////////////////////////////////////////////////////////////////
-
-This script will:
-
-1. Load the given template source file, which contains a placeholder string
-   indicating where to inject the CSS contents;
-2. Load the contents of the given CSS file in memory;
-3. Replace the placeholder string with the CSS contents (inject them);
-4. Save the final result to the given destination file.
-
-*/
-
 templateFile  = "src/GitHub_source.html5" // Template with CSS-injection placeholder
 cssFile       = "src/GitHub.min.css"      // CSS source to inject into placeholder
 outFile       = "GitHub.html5"     // Final template output file
