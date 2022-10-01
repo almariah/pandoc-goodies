bd3b53553ae55f91e93f843e3db0387cc4afb351
Abdullah Almariah <abdullahalmariah@gmail.com>
2022-10-01 12:57:32 +0200

    init

--- obsidian-filter.lua
+++ obsidian-filter.lua
@@ -1,14 +1,22 @@
+local alert_types =
+{
+  ["alertsolid"] = "Alert",
+  ["warningsolid"] = "Warning",
+  ["errorsolid"] = "Error",
+  ["successsolid"] = "Success",
+  ["notesolid"] = "Note",
+}
+
 function BlockQuote (para)
   start = para.content[1]
   if (start.t == "Para" and start.content[1].t == "Str" and
       start.content[1].text:match("^%[!%w+%][-+]?$")) then
       _, _, ctype = start.content[1].text:find("%[!(%w+)%]")
 
-      tags = ctype
+      tags = string.lower(ctype)
+      start.content:remove(1)
 
       title = ""
-
-      start.content:remove(1)
 
       i = 1
       while start.content[i] and start.content[i].tag ~= 'SoftBreak' and start.content[i].tag ~= 'LineBreak'
@@ -18,19 +26,27 @@ function BlockQuote (para)
       end
 
       if (title == "") then
-        title = ctype
+        title = (tags:gsub("^%l", string.upper))
       end
 
       for i=1, i do
         start.content:remove(1)
       end
 
-      blocks = {
-        pandoc.RawInline(
-          'html','<div class="admonition ' .. tags .. '">' ..
-          '<p class="admonition-title">' .. title .. '</p>'
-        ),
-      }
+      blocks = {}
+
+      if (alert_types[tags] ~= nil) then
+        table.insert(blocks, pandoc.RawInline(
+            'html',string.format('<div class="%s">', alert_types[tags])    
+          )
+        )
+      else
+        table.insert(blocks, pandoc.RawInline(
+            'html','<div class="admonition ' .. tags .. '">' ..
+            '<p class="admonition-title">' .. title .. '</p>'
+          )
+        )
+      end
 
       table.insert(blocks, start.content)
 
